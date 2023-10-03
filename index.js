7d6e2aa21f329e4505e3e9878992ac7822d710e9
EzatRashid <ezat.b.rashid@gmail.com>
2023-10-02 21:05:01 -0600

    sum bs

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const appSettings = {
+    databaseURL: 'https://add-to-cart-app-b13ee-default-rtdb.firebaseio.com/'
+}
+
 let inputEl = document.getElementById('input')
 let addBtn = document.getElementById('add')
 
