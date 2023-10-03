c0621258778b0a03aa00c59dc5bac8ca08c9e6f3
EzatRashid <ezat.b.rashid@gmail.com>
2023-10-02 21:08:32 -0600

    sum bs

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+
+const app = initializeApp(appSettings);
+console.log(app)
 const appSettings = {
     databaseURL: 'https://add-to-cart-app-b13ee-default-rtdb.firebaseio.com/'
 }
