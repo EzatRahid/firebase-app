b05f96b0106840cd58b8ab63b5ec4b7c74f2ab2e
EzatRashid <ezat.b.rashid@gmail.com>
2023-10-03 20:28:08 -0600

    sum bs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+import { getDatabase,ref,push } from 'https://www.gstatic.com/firebasejs/firebase-database.js'
 
 const appSettings = {
     databaseURL: 'https://add-to-cart-app-b13ee-default-rtdb.firebaseio.com/'
 }
 const app = initializeApp(appSettings);
+const database = getDatabase(app)
+const moviesInDb = ref(database, "movies")
+
 console.log(app)
 
 let inputEl = document.getElementById('input')
@@ -12,5 +16,6 @@ let addBtn = document.getElementById('add')
 
 addBtn.addEventListener('click', () =>{
     let inputValue = inputEl
+    push(moviesInDb(inputValue))
     console.log(inputValue)
 })
