623e5218e8b56578512abac3e0db5afe4c8366f8
EzatRashid <ezat.b.rashid@gmail.com>
2023-10-05 21:21:58 -0600

    it works

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref , push } from 'https://www.gstatic.com/firebasejs/firebase-database.js'
+import { getDatabase, ref , push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
     databaseURL: 'https://add-to-cart-app-b13ee-default-rtdb.firebaseio.com/'
 }
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
+const shoppingListInDB = ref(database,"shoppingList")
 
 
 console.log(app)
@@ -15,7 +16,8 @@ let addBtn = document.getElementById('add')
 
 
 addBtn.addEventListener('click', () =>{
-    let inputValue = inputEl
-    push(moviesInDb(inputValue))
+    let inputValue = inputEl.value
+    push(shoppingListInDB, inputValue)
     console.log(inputValue)
+    inputValue.value = ''
 }) 
